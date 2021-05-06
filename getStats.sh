55e0a5588ae99dec97b8787e0fcfa4545fcc68ae
snooth <snooth@gmail.com>
2021-05-06 21:08:05 +1000

    Create getStats.sh

---
+++ getStats.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+cd /home/chia/chia-blockchain/
+. ./activate
+echo ""
+echo "***************"
+echo "CHIA HYPERSCALERS NODE"
+echo "***************"
+echo ""
+echo "***************"
+echo "Chia Wallet Show"
+echo "***************"
+echo ""
+chia farm summary
+echo ""
+echo "***************"
+echo "Chia Wallet Show"
+echo "***************"
+echo ""
+chia wallet show
+echo ""
+echo "***************"
+echo "CHIA MAINCHIA NODE"
+echo "***************"
+echo ""
